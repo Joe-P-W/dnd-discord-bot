4e72ae85fe119887b1bbbede254d8582ece525ae
Joe Wasden <jpwasden@gmail.com>
2021-01-12 18:42:27 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dnd_discord_bot-Joe_P_W",
-    version="0.0.1",
+    version="0.0.2",
     author="Joe W",
     author_email="",
     description="A dnd discord bot",
